2560682ad07971c69ab4bf7cddf3ba41ae75dad3
Bharat Gupta <36244814+virat07@users.noreply.github.com>
2023-09-17 19:50:49 -0700

    git ignore added

--- .history/main_20230915121337.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.title("Latest Data Professionals Salary")
-def load_data (): 
-    data = pd.read_csv("/Salary Dataset.csv")
-data = load_data()
