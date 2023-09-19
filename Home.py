d5548e4fc0968475ece6e5dd4ce6a4f8ddd78a8b
Bharat Gupta <36244814+virat07@users.noreply.github.com>
2023-09-18 22:17:43 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import pandas as pd
 import plotly.express as px
