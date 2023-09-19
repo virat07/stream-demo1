f14d45b4910d6598d72a031c0bfdea188f056d6b
Bharat Gupta <36244814+virat07@users.noreply.github.com>
2023-09-19 00:19:04 -0700

    home page updated

--- Home.py
+++ Home.py
@@ -20,25 +20,25 @@ data = load_data()
 st.subheader('Raw data')
 st.write(data)
 
-st.markdown("Histrogram ❄️")
-# st.sidebar.markdown("# Page 2 ❄️")
-st.write(st.session_state["shared"])
+# st.markdown("Histrogram ❄️")
+# # st.sidebar.markdown("# Page 2 ❄️")
+# st.write(st.session_state["shared"])
 
-SALARY_DATA = "Salary"
+# SALARY_DATA = "Salary"
 
-hist_values = np.histogram(
-    data[SALARY_DATA], bins=20)
-st.bar_chart(hist_values)
+# hist_values = np.histogram(
+#     data[SALARY_DATA], bins=20)
+# st.bar_chart(hist_values)
 
 
 
-st.markdown("Scatter Graph ❄️")
-# st.sidebar.markdown("# Page 2 ❄️")
-st.write(st.session_state["shared"])
+# st.markdown("Scatter Graph ❄️")
+# # st.sidebar.markdown("# Page 2 ❄️")
+# st.write(st.session_state["shared"])
 
-fig1 = px.scatter(data, x=data['Salary'], y=data['Location'], color="Salary")
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-with tab1:
-    st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
-with tab2:
-    st.plotly_chart(fig1, theme=None, use_container_width=True)
+# fig1 = px.scatter(data, x=data['Salary'], y=data['Location'], color="Salary")
+# tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+# with tab1:
+#     st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
+# with tab2:
+#     st.plotly_chart(fig1, theme=None, use_container_width=True)
